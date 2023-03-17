d62eb0e2f31efa49b4b3c4d52b81ea6f4bb9067c
Sam <Abdusamadov55@gmail.com>
2023-03-17 19:10:58 +0900

    Test coverage 100% -------

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -12,4 +12,4 @@ def test_slice():
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
     assert arrs.my_slice([], 1) == []
     assert arrs.my_slice([1, 2, 3, 4], -1) == [4]
-    assert arrs.my_slice([1, 2, 3, 4], -5) == [1, 2, 3, 4]
+    assert arrs.my_slice([1, 2, 3, ], -5) == [1, 2, 3, ]
